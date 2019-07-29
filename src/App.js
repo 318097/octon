4d9547e89553c0cf0968c569b6f73d338c6946f1
Mehul <318097@gmail.com>
2019-07-29 19:28:27 +0530

    Add: Axios default URL & Auth header

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import React, { Fragment } from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import 'antd/dist/antd.css';
+import axios from 'axios';
+
 import Home from './Components/home';
 import Expenses from './Components/expenses';
+
+axios.defaults.baseURL = 'http://localhost:3001/api';
+axios.defaults.headers.common['authorization'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZDNlZjVmMjc0MzcxNjRlYTdhMGJhM2UiLCJpYXQiOjE1NjQ0MDcyOTB9.NMMr1avMBlYtZ6hbG1UPy-pvQ56-D1D7UboBrpVHjkM';
 
 function App() {
   return (
