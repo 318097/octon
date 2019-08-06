31da8bf70601d435df2ca61d4b57fa9b43d17168
Mehul <318097@gmail.com>
2019-08-06 20:18:04 +0530

    Add: Navigation links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import './App.css';
+import { Switch, Route, BrowserRouter, Link } from 'react-router-dom';
+import './App.scss';
 import 'antd/dist/antd.css';
 import axios from 'axios';
 
@@ -14,20 +14,25 @@ axios.defaults.headers.common['authorization'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6Ik
 function App() {
   return (
     <Fragment>
-      <div className="App">
-        <header className="App-header">
-          Brainbox
-      </header>
-        <section>
-          <BrowserRouter>
+      <BrowserRouter>
+        <div className="app">
+          <header className="app-header">
+            <h2>Brainbox</h2>
+          </header>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/expenses">Expenses</Link>
+            <Link to="/todos">Todos</Link>
+          </nav>
+          <section>
             <Switch>
               <Route exact path="/expenses" component={Expenses} />
               <Route exact path="/todos" component={Todos} />
               <Route exact path="/" component={Home} />
             </Switch>
-          </BrowserRouter>
-        </section>
-      </div>
+          </section>
+        </div>
+      </BrowserRouter>
     </Fragment>
   );
 }
