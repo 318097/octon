e93da6b25ab0744124028b274f8c899932f3c441
Mehul <318097@gmail.com>
2019-08-06 20:09:37 +0530

    Refactor: Change folder name to `components`

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 import 'antd/dist/antd.css';
 import axios from 'axios';
 
-import Home from './Components/Home';
-import Expenses from './Components/Expenses';
-import Todos from './Components/Todos';
+import Home from './components/Home';
+import Expenses from './components/Expenses';
+import Todos from './components/Todos';
 
 axios.defaults.baseURL = 'http://localhost:3001/api';
 axios.defaults.headers.common['authorization'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZDNlZjVmMjc0MzcxNjRlYTdhMGJhM2UiLCJpYXQiOjE1NjQ0MDcyOTB9.NMMr1avMBlYtZ6hbG1UPy-pvQ56-D1D7UboBrpVHjkM';
