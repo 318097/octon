5d927f69d517e0581d19b49d18eaee03f1b2db76
Mehul <318097@gmail.com>
2019-09-25 23:36:40 +0530

    Refactor: Register page

--- src/components/Register.js
+++ src/components/Register.js
@@ -12,13 +12,15 @@ class Register extends Component {
       username: "",
       password: "",
       email: "",
-      redirect: false
+      redirect: false,
+      loading: false
     };
   }
 
   handleInput = key => e => this.setState({ [key]: e.target.value });
 
   handleRegister = async () => {
+    this.setState({ loading: true });
     const { name, username, password, email } = this.state;
     try {
       await axios.post("/auth/register", {
@@ -28,7 +30,7 @@ class Register extends Component {
         email
       });
 
-      this.setState({ redirect: true });
+      setTimeout(() => this.setState({ redirect: true, loading: false }), 1000);
     } catch (err) {
       message.error(err.message);
       console.log(err);
@@ -36,7 +38,8 @@ class Register extends Component {
   };
 
   render() {
-    if (this.state.redirect) {
+    const { name, username, password, email, redirect, loading } = this.state;
+    if (redirect) {
       return <Redirect to="/login" />;
     }
     return (
@@ -45,25 +48,25 @@ class Register extends Component {
         <form>
           <Input
             className="input"
-            value={this.state.name}
+            value={name}
             onChange={this.handleInput("name")}
             placeholder="Name"
           />
           <Input
             className="input"
-            value={this.state.email}
+            value={email}
             onChange={this.handleInput("email")}
             placeholder="Email"
           />
           <Input
             className="input"
-            value={this.state.username}
+            value={username}
             onChange={this.handleInput("username")}
             placeholder="Username"
           />
           <Input.Password
             className="input"
-            value={this.state.password}
+            value={password}
             onChange={this.handleInput("password")}
             placeholder="Password"
             onPressEnter={this.handleRegister}
@@ -73,6 +76,7 @@ class Register extends Component {
             className="input"
             type="primary"
             onClick={this.handleRegister}
+            loading={loading}
           >
             Register
           </Button>
