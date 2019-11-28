330268c7d225da84934c9daa3e98ff2b97751b27
Mehul <318097@gmail.com>
2019-11-28 20:12:12 +0530

    Refactor: Register into functional component

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,27 +1,20 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Input, Button, message } from "antd";
 import { Redirect } from "react-router-dom";
 
 import axios from "axios";
 
-class Register extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      username: "",
-      password: "",
-      email: "",
-      redirect: false,
-      loading: false
-    };
-  }
+const Register = () => {
+  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  handleInput = key => e => this.setState({ [key]: e.target.value });
+  const [redirect, setRedirect] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  handleRegister = async () => {
-    this.setState({ loading: true });
-    const { name, username, password, email } = this.state;
+  const handleRegister = async () => {
+    setLoading(true);
     try {
       await axios.post("/auth/register", {
         username,
@@ -30,60 +23,58 @@ class Register extends Component {
         email
       });
 
-      setTimeout(() => this.setState({ redirect: true, loading: false }), 1000);
+      setTimeout(() => setRedirect(true), 1000);
     } catch (err) {
-      message.error(err.message);
-      console.log(err);
+      const { data: errorMessage } = err.response;
+      message.error(errorMessage);
+    } finally {
+      setLoading(false);
     }
   };
 
-  render() {
-    const { name, username, password, email, redirect, loading } = this.state;
-    if (redirect) {
-      return <Redirect to="/login" />;
-    }
-    return (
-      <section id="register">
-        <h3 className="text-center"><span className="custom-header">Register</span></h3>
-        <form>
-          <Input
-            className="input"
-            value={name}
-            onChange={this.handleInput("name")}
-            placeholder="Name"
-          />
-          <Input
-            className="input"
-            value={email}
-            onChange={this.handleInput("email")}
-            placeholder="Email"
-          />
-          <Input
-            className="input"
-            value={username}
-            onChange={this.handleInput("username")}
-            placeholder="Username"
-          />
-          <Input.Password
-            className="input"
-            value={password}
-            onChange={this.handleInput("password")}
-            placeholder="Password"
-            onPressEnter={this.handleRegister}
-          />
-          <br />
-          <Button
-            className="input"
-            type="primary"
-            onClick={this.handleRegister}
-            loading={loading}
-          >
-            Register
+  if (redirect) return <Redirect to="/login" />;
+
+  return (
+    <section id="register">
+      <h3 className="text-center"><span className="custom-header">Register</span></h3>
+      <form>
+        <Input
+          className="input"
+          value={name}
+          onChange={({ target: { value } }) => setName(value)}
+          placeholder="Name"
+        />
+        <Input
+          className="input"
+          value={email}
+          onChange={({ target: { value } }) => setEmail(value)}
+          placeholder="Email"
+        />
+        <Input
+          className="input"
+          value={username}
+          onChange={({ target: { value } }) => setUsername(value)}
+          placeholder="Username"
+        />
+        <Input.Password
+          className="input"
+          value={password}
+          onChange={({ target: { value } }) => setPassword(value)}
+          placeholder="Password"
+          onPressEnter={handleRegister}
+        />
+        <br />
+        <Button
+          className="input"
+          type="primary"
+          onClick={handleRegister}
+          loading={loading}
+        >
+          Register
           </Button>
-        </form>
-      </section>
-    );
-  }
+      </form>
+    </section>
+  );
 }
 
 export default Register;
