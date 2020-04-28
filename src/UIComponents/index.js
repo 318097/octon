8307d95c0608afef103c202080e65087c3755fb7
Mehul Lakhanpal <318097@gmail.com>
2020-04-28 14:19:50 +0530

    Update: UI Components listing

--- src/UIComponents/index.js
+++ src/UIComponents/index.js
@@ -2,5 +2,7 @@ import Card from "./Card";
 import Icon from "./Icon";
 import PageHeader from "./PageHeader";
 import Button from "./Button";
+import Tag from "./Tag";
+import Input from "./Input";
 
-export { Card, Icon, PageHeader, Button };
+export { Card, Icon, PageHeader, Button, Tag, Input };
--- src/components/test/ComponentList.js
+++ src/components/test/ComponentList.js
@@ -1,11 +1,34 @@
 import React from "react";
-import { Button } from "../../UIComponents";
+import { Button, Card, Tag, Input } from "../../UIComponents";
 import "./ComponentList.scss";
+
+const UIComponent = ({ type }) => {
+  switch (type) {
+    case "BUTTON":
+      return <Button>Test</Button>;
+    case "CARD":
+      return <Card>Test file</Card>;
+    case "TAG":
+      return <Tag>Test file</Tag>;
+    case "INPUT":
+      return <Input />;
+
+    default:
+      return null;
+  }
+};
+
+const uiList = ["BUTTON", "CARD", "TAG", "INPUT"];
 
 const ComponentList = () => {
   return (
     <div className="ui-container">
-      <Button>Test</Button>
+      {uiList.map((type) => (
+        <div className="item">
+          <UIComponent type={type} />
+          <span className="name">{type}</span>
+        </div>
+      ))}
     </div>
   );
 };
